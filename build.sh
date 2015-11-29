995d9cda35f9ccaa8d24be2a0d8c076d3acbfa5e
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-29 17:55:11 -0500

    fix build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 if hash clang-format 2>/dev/null; then
   clang-format -i hello.cpp
 fi
-if hash clang++ 2>/dev/null; then
-  clang++ -std=c++1y hello.cpp -o mpi-comperf
+if [[ $OSTYPE == darwin* ]]; then
+  mpic++ --std=c++11 hello.cpp -o mpi-comperf
   chmod +x mpi-comperf
 fi
-if hash mpiCC 2>/dev/null; then
+if [[ $OSTYPE == linux* ]]; then
   mpiCC --std=c++11 hello.cpp -o mpi-comperf
   chmod +x mpi-comperf
 fi
