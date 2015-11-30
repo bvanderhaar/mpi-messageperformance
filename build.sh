35ec447ae3fa4411fbab697149039a20f3e95275
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-29 20:58:32 -0500

    re-org

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-if hash clang-format 2>/dev/null; then
-  clang-format -i hello.cpp
-fi
 if [[ $OSTYPE == darwin* ]]; then
-  mpic++ --std=c++11 hello.cpp -o mpi-comperf
-  chmod +x mpi-comperf
+  clang-format -i message-size.cpp message-time.cpp
+  mpic++ --std=c++11 message-size.cpp -o mpi-size
+  mpic++ --std=c++11 message-time.cpp -o mpi-time
+  chmod +x mpi-size
+  chmod +x mpi-time
 fi
 if [[ $OSTYPE == linux* ]]; then
-  mpiCC --std=c++11 hello.cpp -o mpi-comperf
-  chmod +x mpi-comperf
+  mpiCC --std=c++11 message-size.cpp -o mpi-size
+  mpiCC --std=c++11 message-time.cpp -o mpi-time
+  chmod +x mpi-size
+  chmod +x mpi-time
 fi
---
+++ message-time.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <mpi.h>
+
+#define MASTER 0
+#define TAG 0
+#define MSGSIZE 1
+#define MAX 25
+
+int main(int argc, char *argv[]) {
+  int my_rank, source, num_nodes;
+  char my_host[MAX];
+  char message[MSGSIZE];
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_nodes);
+
+  if (my_rank != MASTER) {
+    gethostname(my_host, MAX);
+    ////sprintf(message, "Hello from process %d on host %s!", my_rank, my_host);
+    ////MPI_Send(message, strlen(message) + 1, MPI_CHAR, MASTER, TAG,
+    ////         MPI_COMM_WORLD);
+    double message_sent = MPI_Wtime();
+    MPI_Send(&message_sent, MSGSIZE, MPI_DOUBLE, MASTER, TAG, MPI_COMM_WORLD);
+
+  } else {
+    printf("Num_nodes: %d\n", num_nodes);
+    printf("Hello from Master (process %d)!\n", my_rank);
+    double message_sent = 0;
+    MPI_Status status;
+    for (source = 1; source < num_nodes; source++) {
+      MPI_Recv(&message_sent, MSGSIZE, MPI_LONG_INT, source, TAG,
+               MPI_COMM_WORLD, &status);
+      double message_received = MPI_Wtime();
+      double elapsed = message_received - message_sent;
+      printf("Host: %d Message Ticks: %f Now: %f Elapsed time: %f\n",
+             status.MPI_SOURCE, message_sent, message_received, elapsed);
+    }
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
