e91b09729f8c556344fedfbbb8b045a66daf1c8a
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-29 19:20:32 -0500

    try bigger format

--- hello.cpp
+++ hello.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
       clock_t message_received = clock();
       double elapsed =
           double(message_received - message_sent) / (CLOCKS_PER_SEC / 1000);
-      printf("Message Ticks: %F Now: %F Elapsed time: %f\n", message_sent,
+      printf("Message Ticks: %ll Now: %ll Elapsed time: %f\n", message_sent,
              message_received, elapsed);
     }
   }
