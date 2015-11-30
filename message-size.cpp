fcb6fdee28b648bc780663aea68b0655ce10c891
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-29 21:19:30 -0500

    try proper format

--- message-size.cpp
+++ message-size.cpp
@@ -5,8 +5,8 @@
 
 #define MASTER 0
 #define TAG 0
-#define MSGSIZE 999999
 #define MAX 25
+const long long MSGSIZE = 999999;
 
 int main(int argc, char *argv[]) {
   int my_rank, source, num_nodes;
@@ -15,16 +15,16 @@ int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &num_nodes);
-
+  double program_start = MPI_Wtime();
   if (my_rank != MASTER) {
     double message_sent = MPI_Wtime();
     for (long long i = 0; i < MSGSIZE; i++) {
-      message[i] = 'a' + rand()%26;
+      message[i] = 'a' + rand() % 26;
     }
     MPI_Send(&message, MSGSIZE, MPI_CHAR, MASTER, TAG, MPI_COMM_WORLD);
 
   } else {
-    double program_start = MPI_Wtime();
+
     printf("Num_nodes: %d\n", num_nodes);
     printf("Hello from Master (process %d)!\n", my_rank);
     char message[MSGSIZE];
@@ -32,15 +32,12 @@ int main(int argc, char *argv[]) {
     for (source = 1; source < num_nodes; source++) {
       MPI_Recv(&message, MSGSIZE, MPI_CHAR, source, TAG, MPI_COMM_WORLD,
                &status);
-      ////double message_received = MPI_Wtime();
-      ////double elapsed = message_received - message_sent;
-      ////printf("Host: %d Message Ticks: %f Now: %f Elapsed time: %f\n",
-      ////       status.MPI_SOURCE, message_sent, message_received, elapsed);
     }
 
     double program_end = MPI_Wtime();
     double program_elapsed = program_end - program_start;
-    printf("Nodes, Message Size, Program Execution Time, %f,%f,%f\n", num_nodes, MSGSIZE, program_elapsed);
+    printf("Nodes, Message Size, Program Execution Time, %d,%lld,%f\n",
+           num_nodes, MSGSIZE, program_elapsed);
   }
 
   MPI_Finalize();
