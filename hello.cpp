0a544828f7c6a6d631bdcf54c5771bb28daf30da
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-29 19:44:23 -0500

    try wtime

--- hello.cpp
+++ hello.cpp
@@ -22,19 +22,18 @@ int main(int argc, char *argv[]) {
     ////sprintf(message, "Hello from process %d on host %s!", my_rank, my_host);
     ////MPI_Send(message, strlen(message) + 1, MPI_CHAR, MASTER, TAG,
     ////         MPI_COMM_WORLD);
-    long int message_sent = (long int) clock();
-    MPI_Send(&message_sent, 1, MPI_LONG_INT, MASTER, TAG, MPI_COMM_WORLD);
+    double message_sent = MPI_Wtime();
+    MPI_Send(&message_sent, 1, MPI_DOUBLE, MASTER, TAG, MPI_COMM_WORLD);
 
   } else {
     printf("Num_nodes: %d\n", num_nodes);
     printf("Hello from Master (process %d)!\n", my_rank);
-    long int message_sent = 0;
+    double message_sent = 0;
     for (source = 1; source < num_nodes; source++) {
       MPI_Recv(&message_sent, MSGSIZE, MPI_LONG_INT, source, TAG,
                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      clock_t message_received = clock();
-      double elapsed =
-          double(message_received - message_sent) / (CLOCKS_PER_SEC / 1000);
+      double message_received = MPI_Wtime();
+      double elapsed = message_received - message_sent;
       printf("Message Ticks: %f Now: %f Elapsed time: %f\n", message_sent,
              message_received, elapsed);
     }
